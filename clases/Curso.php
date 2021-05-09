939d3a42cf952674f0b45f1dab88f05b2cd70bdb
Luis Antonio Gutierrez Castro <lantonio.gc99@gmail.com>
2021-05-09 16:47:59 -0500

    Insert añadido a la clase control
    Se añade el insert a la clase control de curso
    Mejora al método buscarCursoNombre para que  siempre compare las cadenas en minusculas y asi no haya errores de no coincidir por mayusculas

--- clases/Curso.php
+++ clases/Curso.php
@@ -37,7 +37,7 @@
             $SQL_Bus_Curso =
             "   SELECT CURS_ID_CURSOS, CURS_NOMBRE, CURS_TIPO, CURS_NUM_SESIONES, CURS_ACTIVO, CURS_NIVEL, CURS_REQ_TECNICOS, CURS_CONOCIMIENTOS, CURS_OBJETIVOS,CURS_TEMARIO
                 FROM CURSO
-                WHERE CURS_NOMBRE = $curso;
+                WHERE LOWER(CURS_NOMBRE) = LOWER($curso);
             ";
 
             $bd = new BD();
--- modulos/Control_Curso.php
+++ modulos/Control_Curso.php
@@ -26,130 +26,29 @@
 
   //! Todo lo siguiente viene de Usuarios, se debe cambiar conforme se avance a cursos
 
-  /*
-  if($_POST['dml'] == 'insert')
-  {
-    $nombreUsu = $_POST['strNombreUsuario'];
-    $usuario_existente = $obj_Usuario->buscarNombreUsuario($nombreUsu);
-    
-    if(!isset($usuario_existente->usua_num_usuario)) {
+  if($_POST['dml'] == 'insert'){
+    $nombre = $_POST['strNombreCurso'];
+    $cursoExistente = $obj_Curso->buscarCursoNombre($nombre);
+
+    if(!isset($cursoExistente->curs_nombre)){
       
-      //?Datos de persona
-      $nombre = $_POST['strUsuarioNombre'];
-      $apellidoPaterno = $_POST['strUsuarioPrimerApe'];
-      $apellidoMaterno = $_POST['strUsuarioSegundoApe'];
-      $correo = $_POST['strUsuarioCorreo'];
-      $telefono = $_POST['strUsuarioTelefono'];
-      
-      //? Datos de usuario
-      $nombreUsuario = $_POST['strNombreUsuario'];
-      $rol = (integer) $_POST['intUsuarioRol'];
-      $pregunta = (integer) $_POST['UsuarioPregunta'];
-      $recuperacion = $_POST['UsuarioRespuesta'];
-      $contrasenia = $_POST['strContrasenia01'];
-      $estado = isset($_POST['bEstado']) ? $_POST['bEstado'] : TRUE; //? El admin los crea en activo, debo poner en profesor el estado como false
-      
-      //?Datos según rol
-      
-      switch($rol){
-        case 1: //Administrador
-          $num_trabajador = $_POST['intNum_Trabajador'];
-          $rfc = $_POST['strRFC'];
-        break;  
+      //Datos del curso
+      $nombre = $_POST['strNombreCurso'];
+      $tipo = $_POST['intTipoCurso'];
+      $nivel = $_POST['intNivel'];
+      $req_tecnicos = $_POST['strReqTec'];
+      $objetivos = $_POST['strObjCurso'];
+      $conocimientos = $_POST['strConNeces'];
+      $num_sesiones = $_POST['strNumeroSesiones'];
+      $temario = null;//Funcional en lo que se logra crear el directorio para los archivos y guardar solo la url
 
-        case 2: //Moderador
-          //*? Creado para guardar los inputs. Solo guarda los que tienen algo
-          $diasModerador=array();
-          foreach($arr_dias as $dia){
-            static $bandera =  1; 
-            
-            if(isset($_POST['strDiaServicio'.$bandera]) && $_POST['strDiaServicio'.$bandera] != "" ) {
-              $diasModerador[$bandera] = $_POST['strDiaServicio'.$bandera];
-            } 
-            $bandera++;
-          }
-
-          $num_cuenta = $_POST['lbNumCuenta'];
-          $fechaInicio = $_POST['strFechaInicio'];
-          $fechaFin = $_POST['strFechaFin']; 
-          $horaInicio = $_POST['strHoraInicio']; 
-          $horaFin = $_POST['strHoraFin'];
-        break; 
-
-        case 3: //Profesor
-          $num_trabajador = $_POST['intNum_Trabajador'];
-          $rfc = $_POST['strRFC'];
-          $semblanza = $_POST['strSemblanza'];
-          //*? Creado para guardar los inputs. Solo guarda los que tienen algo
-          $nivelesProfesor=array();
-          $bandera =  1; 
-          foreach($arr_niveles as $nivel){
-            if(isset($_POST['strNivel'.$bandera]) && $_POST['strNivel'.$bandera] != "" ) {
-              $nivelesProfesor[$bandera] = $_POST['strNivel'.$bandera];
-            } 
-            $bandera++;
-          }
-
-          $modalidadesProfesor=array();
-          $bandera =  1; 
-          foreach($arr_modalidades as $modalidad){
-            if(isset($_POST['strModalidad'.$bandera]) && $_POST['strModalidad'.$bandera] != "" ) {
-              $modalidadesProfesor[$bandera] = $_POST['strModalidad'.$bandera];
-            } 
-            $bandera++;
-          }
-
-          $coordinacionesProfesor=array();
-          $bandera =  1; 
-          foreach($arr_coordinaciones as $coordinacion){
-            if(isset($_POST['strCoordinacion'.$bandera]) && $_POST['strCoordinacion'.$bandera] != "" ) {
-              $coordinacionesProfesor[$bandera] = $_POST['strCoordinacion'.$bandera];
-            } 
-            $bandera++;
-          }
-        break; 
-      }
-
-      $obj_Persona->agregarPersona($nombre, $apellidoPaterno, $apellidoMaterno, $correo, $telefono);
-      $persona = $obj_Persona->buscarUltimo();
-
-      $obj_Usuario->agregarUsuario($persona, $rol, $pregunta, $nombreUsuario, $contrasenia, $recuperacion, $estado);
-      
-      switch($rol){
-        case 1: //Administrador
-          $obj_Administrador->agregarAdministrador($persona, $num_trabajador, $rfc);
-        break;
-
-        case 2: //Moderador
-          $obj_Moderador->agregarModerador($persona, $num_cuenta, $fechaInicio, $fechaFin, $horaInicio, $horaFin );
-          foreach($diasModerador as $id){
-            $obj_Moderador->agregarDiasModerador($persona, $id);
-          }
-        break;
-
-        case 3: //Profesor
-          $obj_Profesor->agregarProfesor($persona, $num_trabajador, $semblanza, $rfc);
-          
-          foreach($nivelesProfesor as $id){
-            $obj_Profesor->agregarNivelesProfesor($persona, $id);
-          }
-          foreach($modalidadesProfesor as $id){
-            $obj_Profesor->agregarModalidadesProfesor($persona, $id);
-          }
-          foreach($coordinacionesProfesor as $id){
-            $obj_Profesor->agregarCoordinacionesProfesor($persona, $id);
-          }
-        break;
-      }
-      
-
+      $obj_Curso->agregarCurso($tipo, $nombre, $num_sesiones, $req_tecnicos, $conocimientos, $nivel, $objetivos, );
       echo 1;
+
     } else {
       echo 2;
     }
-      
   }
-  else*/
   
   if($_POST['dml'] == 'update')
   {
