aaf3757991974a6d7f7210d3505e317d22634a26
Samuel <44588639+SamuelAc1011@users.noreply.github.com>
2021-07-10 18:22:29 -0500

    Completo sección: Mi cuenta

--- clases/BD.php
+++ clases/BD.php
@@ -2,186 +2,179 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-	class BD
-	{
-		public $servidor;
-		public $usuario;
-		public $password;
-		public $puerto;
-		public $bd;
-		public $conexion;
-		public $cadena_conexion;
-		public $imagen;
+class BD
+{
+    public $servidor;
+    public $usuario;
+    public $password;
+    public $puerto;
+    public $bd;
+    public $conexion;
+    public $cadena_conexion;
+    public $imagen;
 
-		function __construct($strBD = "")
-		{
-			//    require('../include/Configuracion.php');
+    function __construct($strBD = "")
+    {
+        //    require('../include/Configuracion.php');
 
-			$this->servidor = "localhost";
-			$this->usuario  = "postgres";
-			$this->password = "postgres";
-			$this->puerto   = "5432";
-			$this->bd       = ($strBD != "") ? $strBD : "SAPPCDP2";
-			$this->cadena_conexion = "host=". $this->servidor ." port=". $this->puerto ." dbname=". $this->bd ." user=". $this->usuario ." password=". $this->password;
-		}
+        $this->servidor = "localhost";
+        $this->usuario  = "postgres";
+        $this->password = "postgres";
+        $this->puerto   = "5432";
+        $this->bd       = ($strBD != "") ? $strBD : "SAPPCDP2";
+        $this->cadena_conexion = "host=". $this->servidor ." port=". $this->puerto ." dbname=". $this->bd ." user=". $this->usuario ." password=". $this->password;
+    }
 
-		function abrirBD()
-		{
-			if ($this->conexion = pg_connect($this->cadena_conexion))
-			{
-				return $this->conexion;
-			}
-			else
-			{
-				return false;
-			}
-		}
+    function abrirBD()
+    {
+        if ($this->conexion = pg_connect($this->cadena_conexion)) {
+            return $this->conexion;
+        } else {
+            return false;
+        }
+    }
 
-		function cerrarBD()
-		{
-			pg_close($this->conexion);
-		}
-	}
+    function cerrarBD()
+    {
+        pg_close($this->conexion);
+    }
+}
 
-	class Transaccion
-	{
-		public $conexion;
-		public $query;
-		public $queryasincrono;
-		public $error;
-		public $oid;
-		public $imagen;
+class Transaccion
+{
+    public $conexion;
+    public $query;
+    public $queryasincrono;
+    public $error;
+    public $oid;
+    public $imagen;
 
-		function __construct($conexion)
-		{
-			$this->conexion = $conexion;
-		}
+    function __construct($conexion)
+    {
+        $this->conexion = $conexion;
+    }
 
-	/**************************************************************
-	  * Método que ejecuta la instrucción DML
-	  **************************************************************/
-		function enviarQuery($sql)
-		{
-			//require('../../include/Configuracion.php');
-			$this->query = pg_query($this->conexion, $sql);
-			//if ($_conf_show_sql == "on") {
-				//$handle = fopen($rutainstalacion . "logs/querys.log", "a+");
-				//$edoquery1 = pg_result_status($this->query, PGSQL_STATUS_STRING);
-				//$edoquery2 = pg_result_status($this->query, PGSQL_STATUS_LONG);
-				//$fecsis = date("d M Y, H:i:s");
-				//fwrite($handle, $fecsis . " \n" . $sql . "\n" . $edoquery1 . " " . $edoquery2 . " " . $_arr_error_pgsql[$edoquery2] . "\n\n");
-				//fclose($handle);
-			//}
-			if ($this->query)
-			{
-				$this->oid = pg_last_oid($this->query); // Lalo: ¿Qué es oid?
-				return $this->query;
-			}
-			else
-			{
-				return false;
-			}
-		}
+    /**************************************************************
+      * Método que ejecuta la instrucción DML
+      **************************************************************/
+    function enviarQuery($sql)
+    {
+        //require('../../include/Configuracion.php');
+        $this->query = pg_query($this->conexion, $sql);
+        //if ($_conf_show_sql == "on") {
+            //$handle = fopen($rutainstalacion . "logs/querys.log", "a+");
+            //$edoquery1 = pg_result_status($this->query, PGSQL_STATUS_STRING);
+            //$edoquery2 = pg_result_status($this->query, PGSQL_STATUS_LONG);
+            //$fecsis = date("d M Y, H:i:s");
+            //fwrite($handle, $fecsis . " \n" . $sql . "\n" . $edoquery1 . " " . $edoquery2 . " " . $_arr_error_pgsql[$edoquery2] . "\n\n");
+            //fclose($handle);
+        //}
+        if ($this->query) {
+            $this->oid = pg_last_oid($this->query); // Lalo: ¿Qué es oid?
+            return $this->query;
+        } else {
+            return false;
+        }
+    }
 
 
-	/**************************************************************
-	   * Método que envía una serie de querys en forma simultanea,
-	   * tipo transaccion en una base de datos.
-	  **************************************************************/
-		function enviarQueryAsincrono($sqls, $numsqls) // Lalo: ¿Qué es un Query Asincrono?
-		{
-			pg_set_error_verbosity($this->conexion, PGSQL_ERRORS_VERBOSE);
-			pg_send_query($this->conexion, $sqls);
-			for ($i = 0; $i <= $numsqls; $i++) {
-				$this->query = pg_get_result($this->conexion);
-				$this->error = pg_result_error($this->query);
-				if ($this->error != "") {
-					return false;
-				}
-			}
-		}
-	/**************************************************************
-	   * Método que sirve para hacer un commit a un query asincrono.
-	   * Y el resultado del primer o n query que se envío.
-	  **************************************************************/
-		function traerResultadoQueryAsincrono()
-		{
-			$this->query = pg_get_result($this->conexion);
-		}
-	/**************************************************************
-	   * Método que sirve para mostrar el error generado por un query.
-	   * @attribute error se guarda el error que mando el query.
-	  **************************************************************/
-		function traerErrorQueryAsincrono()
-		{
-			return  $this->error;
-		}
+    /**************************************************************
+       * Método que envía una serie de querys en forma simultanea,
+       * tipo transaccion en una base de datos.
+      **************************************************************/
+    function enviarQueryAsincrono($sqls, $numsqls) // Lalo: ¿Qué es un Query Asincrono?
+    {
+        pg_set_error_verbosity($this->conexion, PGSQL_ERRORS_VERBOSE);
+        pg_send_query($this->conexion, $sqls);
+        for ($i = 0; $i <= $numsqls; $i++) {
+            $this->query = pg_get_result($this->conexion);
+            $this->error = pg_result_error($this->query);
+            if ($this->error != "") {
+                return false;
+            }
+        }
+    }
+    /**************************************************************
+       * Método que sirve para hacer un commit a un query asincrono.
+       * Y el resultado del primer o n query que se envío.
+      **************************************************************/
+    function traerResultadoQueryAsincrono()
+    {
+        $this->query = pg_get_result($this->conexion);
+    }
+    /**************************************************************
+       * Método que sirve para mostrar el error generado por un query.
+       * @attribute error se guarda el error que mando el query.
+      **************************************************************/
+    function traerErrorQueryAsincrono()
+    {
+        return  $this->error;
+    }
 
-	/**************************************************************
-	   * Método que realiza retorna el array con los registros
-	   * obtenidos
-	  **************************************************************/
-		function traerRegistro($i)
-		{
-			return pg_fetch_array($this->query,$i);
-		}
+    /**************************************************************
+       * Método que realiza retorna el array con los registros
+       * obtenidos
+      **************************************************************/
+    function traerRegistro($i)
+    {
+        return pg_fetch_array($this->query, $i);
+    }
 
-		function traerRegistros()
-		{
-			if (pg_num_rows($this->query) == 0){
-				return NULL;
-			} else {
-				return pg_fetch_all($this->query);
-			}
-		}
+    function traerRegistros()
+    {
+        if (pg_num_rows($this->query) == 0) {
+            return null;
+        } else {
+            return pg_fetch_all($this->query);
+        }
+    }
 
 
-	/**************************************************************
-	   * Método que retorna el array con los registros
-	   * obtenidos en forma de objeto
-		**************************************************************/
-		function traerObjeto($i)
-		{
-			if (pg_num_rows($this->query) == 0){
-				return NULL;
-			} else {
-				return pg_fetch_object($this->query,$i);
-			}
-		}
+    /**************************************************************
+       * Método que retorna el array con los registros
+       * obtenidos en forma de objeto
+        **************************************************************/
+    function traerObjeto($i)
+    {
+        if (pg_num_rows($this->query) == 0) {
+            return null;
+        } else {
+            return pg_fetch_object($this->query, $i);
+        }
+    }
 
-	/**************************************************************
-	   * Método que realiza retorna el nÃºmero de registros afectados
-	   **************************************************************/
-		function traerRegistrosAfectados()
-		{
-			return pg_affected_rows($this->query);
-		}
+    /**************************************************************
+       * Método que realiza retorna el nÃºmero de registros afectados
+       **************************************************************/
+    function traerRegistrosAfectados()
+    {
+        return pg_affected_rows($this->query);
+    }
 
-	/**************************************************************
-	   * Método que realiza retorna el numero de registros de la
-	   * consulta
-	   *************************************************************/
+    /**************************************************************
+       * Método que realiza retorna el numero de registros de la
+       * consulta
+       *************************************************************/
 
-		function contarNumeroRegistros()
-		{
-			return pg_num_rows($this->query);
-		}
+    function contarNumeroRegistros()
+    {
+        return pg_num_rows($this->query);
+    }
 
-	/**************************************************************
-	   * Método que realiza utiliza el mysql_result() para obtener un
-	   * resultado VERIFICAR?
-	   **************************************************************/
-		function traerCampo($rs, $numero, $campo)
-		{
-			return pg_fetch_result($rs, $numero, $campo);
-		}
+    /**************************************************************
+       * Método que realiza utiliza el mysql_result() para obtener un
+       * resultado VERIFICAR?
+       **************************************************************/
+    function traerCampo($rs, $numero, $campo)
+    {
+        return pg_fetch_result($rs, $numero, $campo);
+    }
 
-	/**************************************************************
-	   * Método que permite obtener el resultado de query()
-	   **************************************************************/
-		function traerQuery()
-		{
-			return $this->query;
-		}
-	}
-?>
+    /**************************************************************
+       * Método que permite obtener el resultado de query()
+       **************************************************************/
+    function traerQuery()
+    {
+        return $this->query;
+    }
+}
--- clases/Usuario.php
+++ clases/Usuario.php
@@ -1,154 +1,155 @@
 <?php
-	class Usuario
-  	{
-		//Actualizar el estatus dado el id y el estatus
-		//? Verificado en la BD 02/07/2021
-		function modificarEstatus($usuario, $estatus)
-		{
-			$SQL_Persona_Est=
-			"
+class Usuario
+{
+    //Actualizar el estatus dado el id y el estatus
+    //? Verificado en la BD 02/07/2021
+    function modificarEstatus($usuario, $estatus)
+    {
+        $SQL_Persona_Est=
+        "
 				UPDATE Usuario
 				SET usua_activo = $estatus
 				WHERE usua_id_usuario = $usuario
 			";
 
 
-			$bd = new BD();
-			$bd->abrirBD();
-			$transaccion_1 = new Transaccion($bd->conexion);
-			$transaccion_1->enviarQuery($SQL_Persona_Est);
-			$bd->cerrarBD();
-		}
+        $bd = new BD();
+        $bd->abrirBD();
+        $transaccion_1 = new Transaccion($bd->conexion);
+        $transaccion_1->enviarQuery($SQL_Persona_Est);
+        $bd->cerrarBD();
+    }
 
-		//Agregar un usuario
-		//? Verificado en la BD 02/07/2021
-		//? La original usua_respuesta ($recuperacion) ya no se incluye en la nueva versión de BD
-		function agregarUsuario($persona, $rol, $pregunta, $nombreUsuario, $contrasenia, $estado)
-    	{	//? El id de la pregunta es NULL por que aún no tenemos nada en esos catálogos
-			$SQL_Ins_Usuario =
-			"	
+    //Agregar un usuario
+    //? Verificado en la BD 02/07/2021
+    //? La original usua_respuesta ($recuperacion) ya no se incluye en la nueva versión de BD
+    function agregarUsuario($persona, $rol, $pregunta, $nombreUsuario, $contrasenia, $estado)
+    {
+   //? El id de la pregunta es NULL por que aún no tenemos nada en esos catálogos
+        $SQL_Ins_Usuario =
+        "	
 				INSERT INTO usuario (pers_id_persona, rol_id_rol, prse_id_pregunta, usua_num_usuario, usua_contrasena, usua_activo)
 				VALUES ($persona, $rol, null, '$nombreUsuario', '$contrasenia', '$estado');
 			";
 
-			$bd = new BD();
-			$bd->abrirBD();
-			$transaccion_1 = new Transaccion($bd->conexion);
-			$transaccion_1->enviarQuery($SQL_Ins_Usuario);
-			$bd->cerrarBD();
-		}
+        $bd = new BD();
+        $bd->abrirBD();
+        $transaccion_1 = new Transaccion($bd->conexion);
+        $transaccion_1->enviarQuery($SQL_Ins_Usuario);
+        $bd->cerrarBD();
+    }
 
-		//Actualizar un usuario dado el id de la persona
-		//? Verificado en la BD 02/07/2021
-		function actualizarUsuario($idPersona, $rol, $pregunta, $nombreUsuario, $contrasenia)
-		{
-			$SQL_Act_Usuario= 
-			" 	
+    //Actualizar un usuario dado el id de la persona
+    //? Verificado en la BD 02/07/2021
+    function actualizarUsuario($idPersona, $rol, $pregunta, $nombreUsuario, $contrasenia)
+    {
+        $SQL_Act_Usuario=
+        " 	
 			UPDATE Usuario
 			SET rol_id_rol = $rol , prse_id_pregunta= null, usua_num_usuario= '$nombreUsuario', usua_contrasena= '$contrasenia'
 			WHERE pers_id_persona = $idPersona AND rol_id_rol = $rol;
 			";
 
-			$bd = new BD();
-			$bd->abrirBD();
-			$transaccion_1 = new Transaccion($bd->conexion);
-			$transaccion_1->enviarQuery($SQL_Act_Usuario);
-			$bd->cerrarBD();
-		}
+        $bd = new BD();
+        $bd->abrirBD();
+        $transaccion_1 = new Transaccion($bd->conexion);
+        $transaccion_1->enviarQuery($SQL_Act_Usuario);
+        $bd->cerrarBD();
+    }
 
-		//Eliminar el resgitro de un usuario dado el id de usuario
-		//? Verificado en la BD 02/07/2021
-		function eliminarUsuario($usuario)
-		{
-			$SQL_Eli_Usuario= 
-			" 	
+    //Eliminar el resgitro de un usuario dado el id de usuario
+    //? Verificado en la BD 02/07/2021
+    function eliminarUsuario($usuario)
+    {
+        $SQL_Eli_Usuario=
+        " 	
 				DELETE FROM usuario
 				WHERE usua_id_usuario = $usuario;
 			";
 
-			$bd = new BD();
-			$bd->abrirBD();
-			$transaccion_1 = new Transaccion($bd->conexion);
-			$transaccion_1->enviarQuery($SQL_Eli_Usuario);
-			$bd->cerrarBD();
-		}
+        $bd = new BD();
+        $bd->abrirBD();
+        $transaccion_1 = new Transaccion($bd->conexion);
+        $transaccion_1->enviarQuery($SQL_Eli_Usuario);
+        $bd->cerrarBD();
+    }
 
-		//Buscar todoas los usuarios
-		//? Verificado en la BD 02/07/2021
-		//? La original usua_respuesta y prse_pregunta ya no se incluye en la nueva versión de BD
-		function buscarTodosUsuarios()
-		{
-			$SQL_Bus_usuarios =
-			"	
+    //Buscar todoas los usuarios
+    //? Verificado en la BD 02/07/2021
+    //? La original usua_respuesta y prse_pregunta ya no se incluye en la nueva versión de BD
+    function buscarTodosUsuarios()
+    {
+        $SQL_Bus_usuarios =
+        "	
 				SELECT U.usua_id_usuario,P.pers_id_persona, P.pers_nombre, P.pers_apellido_paterno, P.pers_apellido_materno,U.rol_id_rol, U.usua_num_usuario, R.rol_nombre, U.usua_activo
 				FROM persona P, usuario U , rol R
 				WHERE P.pers_id_persona = U.pers_id_persona AND R.rol_id_rol = U.rol_id_rol
 				ORDER BY U.usua_activo, P.pers_id_persona DESC, R.rol_id_rol
 			";
 
-			$bd = new BD();
-			$bd->abrirBD();
-			$transaccion_1 = new Transaccion($bd->conexion);
-			$transaccion_1->enviarQuery($SQL_Bus_usuarios);
-			$bd->cerrarBD();
-			return ($transaccion_1->traerRegistros());
-		}
+        $bd = new BD();
+        $bd->abrirBD();
+        $transaccion_1 = new Transaccion($bd->conexion);
+        $transaccion_1->enviarQuery($SQL_Bus_usuarios);
+        $bd->cerrarBD();
+        return ($transaccion_1->traerRegistros());
+    }
 
-		//Buscar un usuario dado el id de usuario
-		//? Verificado en la BD 02/07/2021
-		//? La original prse_id_pregunta ya no se incluye en la nueva versión de BD
-		function buscarUsuario($intIdUsuario)
-		{
-			$SQL_Bus_Usuario = 
-			"	
+    //Buscar un usuario dado el id de usuario
+    //? Verificado en la BD 02/07/2021
+    //? La original prse_id_pregunta ya no se incluye en la nueva versión de BD
+    function buscarUsuario($intIdUsuario)
+    {
+        $SQL_Bus_Usuario =
+        "	
 				SELECT U.usua_id_usuario, U.rol_id_rol, R.rol_nombre, U.usua_num_usuario, U.usua_contrasena, U.usua_activo
 				FROM Rol R, Usuario U
 				WHERE R.rol_id_rol = U.rol_id_rol AND U.usua_id_usuario = $intIdUsuario;
 			";
 
-			$bd = new BD();
-			$bd->abrirBD();
-			$transaccion_1 = new Transaccion($bd->conexion);
-			$transaccion_1->enviarQuery($SQL_Bus_Usuario);
-			$obj_Usuario = $transaccion_1->traerObjeto(0);
-			$bd->cerrarBD();
-			return ($transaccion_1->traerObjeto(0));
-		}
+        $bd = new BD();
+        $bd->abrirBD();
+        $transaccion_1 = new Transaccion($bd->conexion);
+        $transaccion_1->enviarQuery($SQL_Bus_Usuario);
+        $obj_Usuario = $transaccion_1->traerObjeto(0);
+        $bd->cerrarBD();
+        return ($transaccion_1->traerObjeto(0));
+    }
 
-		//Bucar datos de un usuario dado el nombre de usuario
-		//? Verificado en la BD 02/07/2021
-		//? La original usua_respuesta y prse_pregunta ya no se incluye en la nueva versión de BD
-		function buscarNombreUsuario($nombreUsu)
-		{
-			$SQL_Bus_Usuario = 
-			"	
+    //Bucar datos de un usuario dado el nombre de usuario
+    //? Verificado en la BD 02/07/2021
+    //? La original usua_respuesta y prse_pregunta ya no se incluye en la nueva versión de BD
+    function buscarNombreUsuario($nombreUsu)
+    {
+        $SQL_Bus_Usuario =
+        "	
 				SELECT U.usua_id_usuario, U.rol_id_rol, U.usua_num_usuario, U.usua_contrasena
 				FROM Rol R, Usuario U
 				WHERE R.rol_id_rol = U.rol_id_rol AND U.usua_num_usuario = '$nombreUsu';
 			";
 
-			$bd = new BD();
-			$bd->abrirBD();
-			$transaccion_1 = new Transaccion($bd->conexion);
-			$transaccion_1->enviarQuery($SQL_Bus_Usuario);
-			$obj_Usuario = $transaccion_1->traerObjeto(0);
-			$bd->cerrarBD();
-			return ($transaccion_1->traerObjeto(0));
-		}
+        $bd = new BD();
+        $bd->abrirBD();
+        $transaccion_1 = new Transaccion($bd->conexion);
+        $transaccion_1->enviarQuery($SQL_Bus_Usuario);
+        $obj_Usuario = $transaccion_1->traerObjeto(0);
+        $bd->cerrarBD();
+        return ($transaccion_1->traerObjeto(0));
+    }
 
-		//Eliminar registro de una persona dado el id de usuario
-		//? Verificado en la BD 02/07/2021
-		function eliminarPersonaRol($intIdPersonaRol){
-			$SQLDEL_PersonaRol = 
-			"
+    //Eliminar registro de una persona dado el id de usuario
+    //? Verificado en la BD 02/07/2021
+    function eliminarPersonaRol($intIdPersonaRol)
+    {
+        $SQLDEL_PersonaRol =
+        "
 				DELETE FROM usuario
 				WHERE usua_id_usuario =  $intIdPersonaRol
 			";
-			$bd = new BD();
-			$bd->abrirBD();
-			$transaccion_1 = new Transaccion($bd->conexion);
-			$transaccion_1->enviarQuery($SQLDEL_PersonaRol);
-			$bd->cerrarBD();
-		}
-	}
-?>
+        $bd = new BD();
+        $bd->abrirBD();
+        $transaccion_1 = new Transaccion($bd->conexion);
+        $transaccion_1->enviarQuery($SQLDEL_PersonaRol);
+        $bd->cerrarBD();
+    }
+}
