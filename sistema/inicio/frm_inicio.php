d97359bc099e8e73c2d1145c3571fd10100bc475
Samuel <44588639+SamuelAc1011@users.noreply.github.com>
2021-07-16 18:34:33 -0500

    Arreglo de Carousel

--- sistema/inicio/frm_inicio.php
+++ sistema/inicio/frm_inicio.php
@@ -1,5 +1,4 @@
-
-  <div id="wrapper">
+<div id="wrapper">
     <div id="content-wrapper">
       <div class="container-fluid">
 
@@ -11,37 +10,33 @@
               <i id="btn_inicio" class="fas fa-home"></i>&nbsp; Inicio
             </a>
           </li>
-         
         </ol>
         <br>
 
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center">
-          
-        <div id="comunicados" class="carousel slide" data-ride="carousel">
-          
-          <ol class="carousel-indicators">
-            <li data-target="#comunicados" data-slide-to="0" class="active"></li>
-            
-            <?php 
-            $comunicados = scandir("../../recursos/imagenes/Carrusel/" , 1);
-            for ($iCont = 1 ; $iCont < count($comunicados) - 2; $iCont ++) { ?> 
-              <li data-target= '#comunicados' data-slide-to= '<?php echo($iCont) ?>'></li> <?php
-            } 
-            ?>
-          </ol>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 " >
+          <div id="comunicados" class="carousel slide spacing_personalizado" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#comunicados" data-slide-to="0" class="active"></li>
+              <?php
+              $comunicados = scandir("../../recursos/imagenes/Carrusel/", 1);
+              for ($iCont = 1; $iCont < count($comunicados) - 2; $iCont ++) { ?> 
+                <li data-target= '#comunicados' data-slide-to= '<?php echo($iCont) ?>'></li> <?php
+              }
+                ?>
+            </ol>
           <div class="carousel-inner">
-          <div class="carousel-item active">
-                <img class="d-block img-fluid w-100"  src="../recursos/imagenes/Carrusel/AAInicial.jpg" alt="imagen0">
-          </div>
+            <div class="carousel-item active fit-image">
+              <img class="d-block img-fluid w-100"  src="../recursos/imagenes/Carrusel/AAInicial.jpg" alt="imagen0">
+            </div>
 
-          <?php 
-            for ($iCont = 1 ; $iCont < count($comunicados) - 2; $iCont ++) { ?>
-              <div class="carousel-item">
-                <img class="d-block img-fluid w-100"  src="../recursos/imagenes/Carrusel/<?php 
-                    echo ($comunicados[$iCont]) ?>" alt='<?php echo("imagen".$iCont) ?>'>
-              </div> <?php
-            } 
-          ?>
+              <?php
+                for ($iCont = 1; $iCont < count($comunicados) - 2; $iCont ++) { ?>
+                  <div class="carousel-item">
+                    <img class="d-block img-fluid w-100 fit-image"  src="../recursos/imagenes/Carrusel/<?php
+                        echo ($comunicados[$iCont]) ?>" alt='<?php echo("imagen".$iCont) ?>'>
+                  </div> <?php
+                }
+                ?>
 
           <a class="carousel-control-prev" href="#comunicados" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -52,8 +47,6 @@
             <span class="sr-only">Next</span>
           </a>
         </div>
-          
-          
         </div>
       </div>
     </div>
