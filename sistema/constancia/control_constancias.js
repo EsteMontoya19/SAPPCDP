f03ff83f857149bfc52a03350b98aa74b43d0a36
EsteMontoya19 <78121395+EsteMontoya19@users.noreply.github.com>
2021-08-12 21:34:02 -0500

    Mejora en el botón de regresar

--- sistema/constancia/control_constancias.js
+++ sistema/constancia/control_constancias.js
@@ -1,17 +1,13 @@
 $(document).ready(function () {
     $('#btn-regresar-constancia').click(function () {
         $('html, body').animate({ scrollTop: 0 }, 0);
-        location.reload();
+        $('#container').load('../sistema/grupos/frm_inicio_grupos.php');
     });
     $('#btn-consultar-constancias').click(function () {
         $('html, body').animate({ scrollTop: 0 }, 0);
         $('#container').load('../sistema/grupos/frm_inicio_grupos.php');
     });
 });
-
-function validarFormularioConstancia() {
-    return true;
-}
 
 function generarConstancia(fechaInicio, fechaFin) {
     var datos = {
@@ -34,34 +30,33 @@ function generarConstancia(fechaInicio, fechaFin) {
 //Insertar Constancia
 $(document).ready(function () {
     $('#btn-registrar-constancia').click(function () {
-        if (validarFormularioConstancia()) {
-            datos = new FormData($('#form_constancias')[0]);
-            $.ajax({
-                type: 'POST',
-                url: '../modulos/Control_Constancia.php',
-                data: datos,
-                contentType: false,
-                processData: false,
+        
+        datos = new FormData($('#form_constancias')[0]);
+        $.ajax({
+            type: 'POST',
+            url: '../modulos/Control_Constancia.php',
+            data: datos,
+            contentType: false,
+            processData: false,
 
-                success: function (respuesta) {
-                    console.log(respuesta);
-                    if (respuesta.endsWith('1')) {
-                        alertify.success('El registro se realizó correctamente');
-                        setTimeout(function () {
-                            $('html, body').animate({ scrollTop: 0 }, 0);
-                            $('#container').load('../sistema/grupos/frm_inicio_grupos.php');
-                        }, 1500);
-                    } else if (respuesta.endsWith('2')) {
-                        alertify.error('Debe seleccionar un archivo .zip');
-                    } else if (respuesta.endsWith('3')) {
-                        alertify.error('Solo se aceptan archivos con extensión .zip');
-                    } else if (respuesta.endsWith('4')) {
-                        alertify.error('Ya se han cargado constancias para este curso.');
-                    }
-                },
-            });
-            return false;
-        }
+            success: function (respuesta) {
+                console.log(respuesta);
+                if (respuesta.endsWith('1')) {
+                    alertify.success('El registro se realizó correctamente');
+                    setTimeout(function () {
+                        $('html, body').animate({ scrollTop: 0 }, 0);
+                        $('#container').load('../sistema/grupos/frm_inicio_grupos.php');
+                    }, 1500);
+                } else if (respuesta.endsWith('2')) {
+                    alertify.error('Debe seleccionar un archivo .zip');
+                } else if (respuesta.endsWith('3')) {
+                    alertify.error('Solo se aceptan archivos con extensión .zip');
+                } else if (respuesta.endsWith('4')) {
+                    alertify.error('Ya se han cargado constancias para este curso.');
+                }
+            },
+        });
+        return false;
     });
 });
 
