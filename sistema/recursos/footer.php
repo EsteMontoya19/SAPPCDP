a7cb330df33576890fddd0779fb245e62c7dec9d
EsteMontoya19 <78121395+EsteMontoya19@users.noreply.github.com>
2022-05-02 00:13:03 -0500

    Feat: Créditos
    Esperemos sea el commit final, se agregaron los créditos en el Footer.

--- sistema/recursos/footer.php
+++ sistema/recursos/footer.php
@@ -1,7 +1,9 @@
     <!-- <footer style="bottom: 0; position: fixed; width:100%;" class="py-1 bg-blue"> -->
     <footer class="py-1 bg-blue">
       <div class="navbar navbar-fixed-bottom">
-        <p style="font-size: 12px;"  class="m-1 text-center text-white smaller">© 2021 Copyright &copy; Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica. De otra forma requiere permiso previo por escrito de la institución. <A HREF="https://www.fca.unam.mx/docs/aviso_privacidad.pdf">AVISO DE PRIVACIDAD. </A> Sitio web administrado por el Centro de Informática de la Facultad de Contaduría y Administración (CIFCA).</p>
+        <p style="font-size: 12px;"  class="m-1 text-center text-white smaller">© 2021 Copyright &copy; Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica. De otra forma requiere permiso previo por escrito de la institución. <A HREF="https://www.fca.unam.mx/docs/aviso_privacidad.pdf">AVISO DE PRIVACIDAD. </A> Sitio web administrado por el Centro de Informática de la Facultad de Contaduría y Administración (CIFCA). 
+          <a href= "#" onclick = "alertify.alert('Elaborado por: ', 'Alcántara Chávez Samuel <br>Fuentez Aguilar Karen <br>Gutiérrez Castro Luis Antonio <br>Montoya Maya Esteban', function(){ alertify.success('Aceptar'); });"> Créditos</a>
+        </p>
       </div>
     </footer>
 
